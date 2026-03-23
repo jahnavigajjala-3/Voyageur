b8a320a306992d375339c71914312a93fd85a9fd
jahnavigajjala3-commits <jahnavigajjala3@gmail.com>
2026-03-23 22:58:21 +0530

    changes in api/route/user file

--- Backend/app/api/v1/routes/user.py
+++ Backend/app/api/v1/routes/user.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter
-from app.db.session import SessionLocal
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.db.session import get_db
 from app.models.user import User
+from app.schemas.user import UserCreate, UserResponse
 
 router = APIRouter()
 
-@router.post("/")
-def create_user(name: str):
-    db = SessionLocal()
-    user = User(name=name)
-    db.add(user)
+@router.post("/users", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
     db.commit()
-    return {"message": "User created"}
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get("/users", response_model=list[UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
