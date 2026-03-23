e9cca86f9b02f6709e47d91a1c713b411e8a1c87
shreya-kindalkar <shreyakindalkar7@gmail.com>
2026-03-23 23:06:07 +0530

    Refactor: Connect API routes and clean main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,19 +3,16 @@ from fastapi import FastAPI
 from app.db.session import engine
 from app.db.base import Base
 from app.models import user  
+from app.api.v1.routes import user as user_routes, trip as trip_routes
 
 app = FastAPI()
 
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(user_routes.router)
+app.include_router(trip_routes.router)
+
 @app.get("/")
 def home():
     return {"message": "API is working"}
-
-@app.get("/users")
-def get_users():
-    return [
-        {"id": 1, "name": "Jahnavi"},
-        {"id": 2, "name": "Meshery Dev"}
-    ]
