3de333dee4454b2b422fa15d3bcb71bf010be7ee
shreya-kindalkar <shreyakindalkar7@gmail.com>
2026-04-05 19:00:09 +0530

    feat(auth): add request and response schemas for user

--- Backend/app/schemas/user.py
+++ Backend/app/schemas/user.py
@@ -4,9 +4,25 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     name: str
     email: str
+    password: str
 
 
 class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    model_config = {
+    "from_attributes": True
+}
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class LoginResponse(BaseModel):
     id: int
     name: str
     email: str
