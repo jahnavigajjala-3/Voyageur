8e83623fc7033e578290eb31de2ddcd19730053e
Shreya <shreyakindalkar7@gmail.com>
2026-05-12 02:54:24 +0530

    Update: Logo in dashboard page

--- Frontend/client/src/components/AppSidebar.jsx
+++ Frontend/client/src/components/AppSidebar.jsx
@@ -60,7 +60,7 @@ export default function AppSidebar() {
           boxShadow: "0 8px 20px -10px rgb(var(--accent-cyan) / 0.5)",
         }}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" style={{ width: "16px", height: "16px" }}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" style={{ width: "16px", height: "16px", transform: "scaleX(-1)" }}>
           <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
         </svg>
       </div>
